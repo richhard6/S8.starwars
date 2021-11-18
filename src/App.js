fb7871534d240874222fb8e3afbbee852934ac8d
Richard Perdomo <richard.perdomo121@gmail.com>
2021-11-18 14:04:32 +0100

    added routing , need to fix routes but the click on the starship takes you to its webpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from './components/home/Home'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import StarshipList from './components/starshipList/StarshipList'
+import Starship from './components/starship/Starship'
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Routes>
+        <Route path="/starships/:name" element={<Starship />}></Route>
+        <Route path="/starshiplist" element={<StarshipList />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/starshipList/StarshipList.js
+++ src/components/starshipList/StarshipList.js
@@ -1,14 +1,18 @@
 import Starship from '../starship/Starship'
+import { Link } from 'react-router-dom'
 
 function StarshipList({ starships }) {
   //aqui se mostrara solo el nombre del starship con una pequeña imagen, y al entrar estara toda la info correspondiente.
   //react router /starships/"x starship"
   console.log(starships.results)
+
   return (
     <div>
       {starships.results &&
         starships.results.map((starship, index) => (
-          <Starship key={index} starship={starship} />
+          <Link to={`starship/${starship.name}`}>
+            <Starship key={index} starship={starship} />
+          </Link>
         ))}
     </div>
   )
