5265269be3d3e72f1784e95f8df0c74c1d2698b3
Richard Perdomo <richard.perdomo121@gmail.com>
2021-11-18 13:20:53 +0100

    first images taken just need to fix something about looking for ALL numbers in string

--- src/components/starship/Starship.js
+++ src/components/starship/Starship.js
@@ -11,15 +11,29 @@ import {
   SMainInfo,
 } from './styles'
 
+import { useState, useEffect } from 'react'
+
 function Starship({ starship }) {
+  const [url, setUrl] = useState('')
+  let starshipImg = starship.url.search(/[0-9]/)
+  useEffect(() => {
+    let starshipNumber = starship.url.charAt(starshipImg)
+    let starshipURL = `https://starwars-visualguide.com/assets/img/starships/${starshipNumber}.jpg`
+    //hay un  problemac on los numeros de dos cifrs.... dsolo pilla el primer numero
+    setUrl((prevUrl) => (prevUrl = starshipURL))
+  }, [starship.url, starshipImg])
+
+  /* `https://starwars-visualguide.com/assets/img/starships/${starshipImg}.jpg` */
   return (
     <Wrapper>
       <SMainInfo>
         <StarshipName>{starship.name} name</StarshipName>
-        <ImageBox>asd</ImageBox>
+        <ImageBox>
+          <img src={url} alt={starship.name} />
+        </ImageBox>
       </SMainInfo>
       <StarshipInfo>
-        <StarshipModel>{starship.model}model</StarshipModel> cursiva
+        <StarshipModel>{starship.model}model</StarshipModel>
         <SManufacturer>{starship.manufacturer} manufacturer</SManufacturer>
         <SPassengers> {starship.passengers} passengers</SPassengers>
         <SClass> {starship.starship_class} starship class</SClass>
