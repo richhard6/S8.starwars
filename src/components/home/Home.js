b7dde4b4bfba74018e9447a02c157c2380977b03
Richard Perdomo <richard.perdomo121@gmail.com>
2022-01-24 17:54:03 +0100

    fixed welcome page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom'
 
+import { Wrapper, Button, Title } from './styles'
+
 function Home() {
   //pimp it n
 
-  return <Link to="/starshiplist">this is the home page start :) </Link>
+  return (
+    <Wrapper>
+      <Title>Welcome to Starwars Starships</Title>
+      <Link to="/starshiplist">
+        <Button>START! </Button>
+      </Link>
+    </Wrapper>
+  )
 }
 
 export default Home
---
+++ src/components/home/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  padding: 1.5rem;
+  color: white;
+  background-color: white;
+  border-radius: 8px;
+  background-color: black;
+  margin-top: 2rem;
+  border: none;
+  font-weight: 900;
+  font-size: 3rem;
+`
+
+export const Title = styled.h1`
+  color: white;
+`
